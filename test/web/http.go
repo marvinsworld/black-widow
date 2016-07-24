735f10a2cba49d21d6b53a5df93fd133a53914fd
zhiqiang.ge <zhiqiang.ge@dmall.com>
2016-07-24 22:00:14 +0800

    goquery

--- test/web/http.go
+++ test/web/http.go
@@ -1,23 +1,33 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
-	"io/ioutil"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func crawl(url string) {
 
-	resp, err := http.Get(url);
+	//resp, err := http.Get(url);
 	//fmt.Println(resp.Body)
 	//fmt.Println(err)
 
-	if err != nil {
-		fmt.Printf("http get response errror=%s\n", err)
-	}
+	//if err != nil {
+	//	fmt.Printf("http get response errror=%s\n", err)
+	//}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Printf("body=%s\n", body)
+	//body, err := ioutil.ReadAll(resp.Body)
+	//fmt.Printf("body=%s\n", body)
+
+	doc, err := goquery.NewDocument("http://sports.sina.com.cn")
+
+	fmt.Println(doc)
+	dhead := doc.Find("head")
+	dcharset := dhead.Find("meta[http-equiv]")
+	charset, _ := dcharset.Attr("content")
+
+	fmt.Println(dhead.Html())
+	fmt.Println(charset)
+	fmt.Println(err)
 }
 
 func main() {
